24d6a86053e21a9ef55784eebadb560018b30dcf
gtpesantren22 <cakromat26@gmail.com>
2024-03-23 20:41:22 +0700

    tambah catatan berkas santri

--- application/models/SantriModel.php
+++ application/models/SantriModel.php
@@ -166,6 +166,9 @@ class SantriModel extends CI_Model
         $this->db->select("tb_santri.nis, nama, desa, kec, kab, catatan, lembaga");
         $this->db->from("tb_santri");
         $this->db->join("berkas_file", 'tb_santri.nis=berkas_file.nis', 'left');
+        $this->db->where("tb_santri.lembaga", '!= MI');
+        $this->db->where("tb_santri.lembaga", '!= RA');
+        $this->db->where("tb_santri.ket", 'baru');
         return $this->db->get();
     }
 }
